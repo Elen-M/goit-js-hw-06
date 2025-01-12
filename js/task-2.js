f66c81258ce49cdbc0767274164a97b347dc4007
Elen-M <meribele15@gmail.com>
2025-01-12 10:26:49 +0200

    new-js-06

--- js/task-2.js
+++ js/task-2.js
@@ -9,15 +9,14 @@ class Storage {
     }
 
     addItem(newItem) {
-        this.#items.push(newItem);
+        return this.#items.push(newItem);
     }
 
     removeItem(itemToRemove) {
-        return this.#items = this.#items.filter(item => item !== itemToRemove);
+        return this.#items=this.#items.filter(item => item !== itemToRemove);
     }
- }
 
-
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
