dbc395c54b5eb52909b7d9cdfbf4bfa33494cb18
Elen-M <meribele15@gmail.com>
2025-01-12 09:53:40 +0200

    js-hw-06

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,15 @@
 class Storage {
     #items;
-    constructor(items) {
-        this.#items = items;
+    constructor(arg) {
+        this.#items = arg;
     }
 
     getItems() {
         return this.#items;
     }
 
-
     addItem(newItem) {
-        return this.#items.push(newItem);
+        this.#items.push(newItem);
     }
 
     removeItem(itemToRemove) {
